cc8247085e34a753c89846a5f109a4d9df9d646a
Hannes Fant <hqnnes@gmail.com>
2024-12-31 14:09:54 +0200

    iconify icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		// @ts-expect-error expects plugin, but gets promise<plugin>
+		sveltekit(),
+		// @ts-expect-error expects plugin, but gets promise<plugin>
+		Icons({
+			compiler: 'svelte'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
