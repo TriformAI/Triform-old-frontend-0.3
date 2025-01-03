41727e6e19efa53695557edf4e3571345342f1b7
Hannes Fant <hqnnes@gmail.com>
2025-01-03 19:23:06 +0200

    tabs & example usage

---
+++ src/lib/components/atoms/Tabs.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  export interface Tab {
+    key: string,
+    label: string
+  }
+
+  let {
+    tabs,
+    activeTab = $bindable()
+  }: {
+    tabs: Tab[],
+    activeTab: Tab | null
+  } = $props()
+
+  const onTabSelect = (tab: Tab) => {
+    activeTab = tab
+  }
+
+  onMount(() => {
+    if (!activeTab) {
+      activeTab = tabs[0]
+    }
+  })
+</script>
+
+<div
+  class="flex flex-row"
+>
+  {#each tabs as tab}
+    {@const isActive = tab.key === activeTab?.key}
+    <button
+      class="
+        transition-all px-6 py-2
+        border-b border-transparent
+        {isActive
+          ? 'border-zinc-300 text-zinc-200'
+          : 'text-zinc-500 hover:text-zinc-400 hover:border-zinc-700'
+        }
+      "
+      onclick={() => onTabSelect(tab)}
+    >
+      {tab.label}
+    </button>
+  {/each}
+</div>
--- src/lib/components/windows/ComponentsToolbox.svelte
+++ src/lib/components/windows/ComponentsToolbox.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import Window from '$lib/components/common/Window.svelte'
 
+  import Tabs from '$lib/components/atoms/Tabs.svelte'
+  import type { Tab } from '$lib/components/atoms/Tabs.svelte'
+
   // Just pass through all props
   const props = $props()
+
+  let activeTab: Tab
 </script>
 
 <Window
@@ -14,5 +19,13 @@
 
   {#snippet body()}
     Temporary body
+
+    <Tabs
+      tabs={[
+        { key: '1', label: 'Actions' },
+        { key: '2', label: 'Agents' }
+      ]}
+      {activeTab}
+    />
   {/snippet}
 </Window>
