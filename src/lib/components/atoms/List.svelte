e7b6277c9cca97197b5cc9105dcf942c56428c17
Hannes Fant <hqnnes@gmail.com>
2025-01-16 08:42:58 +0200

    extract list component

---
+++ src/lib/components/atoms/List.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  
+  const {
+    body
+  }: {
+    body: Snippet
+  } = $props()
+</script>
+
+<div
+  class="absolute right-0 mt-2 w-56 px-2 text-sm text-white bg-[#252627] border border-zinc-700 divide-y divide-zinc-700 rounded-lg shadow focus:outline-none
+      [&>ul]:my-3
+      [&>ul>li]:flex [&>ul>li]:items-center [&>ul>li]:p-3 [&>ul>li]:my-1 [&>ul>li]:rounded-md [&>ul>li]:gap-4
+      [&>ul>li]:transition-colors [&>ul>li]:duration-200 [&>ul>li]:ease-in-out
+      [&>ul>li>button]:w-full [&>ul>li>button]:text-left
+      [&>ul>li:hover]:bg-zinc-600"
+>
+  <!-- Render the body snippet -->
+  {@render body()}
+</div>
--- src/lib/components/common/Dropdown.svelte
+++ src/lib/components/common/Dropdown.svelte
@@ -6,6 +6,7 @@
 	import { fade } from 'svelte/transition'
 
 	import Button from '../atoms/Button.svelte'
+	import List from '../atoms/List.svelte'
 
 	// Destructure props with default values
 	const {
@@ -58,30 +59,10 @@
 	<!-- Dropdown Menu -->
 	{#if $menu.expanded}
 		<div
-			class="absolute right-0 mt-2 w-56 px-2 text-sm text-white bg-[#252627] border border-zinc-700 divide-y divide-zinc-700 rounded-lg shadow focus:outline-none
-           [&>ul]:my-3
-           [&>ul>li]:flex [&>ul>li]:items-center [&>ul>li]:p-3 [&>ul>li]:my-1 [&>ul>li]:rounded-md [&>ul>li]:gap-4
-           [&>ul>li]:transition-colors [&>ul>li]:duration-200 [&>ul>li]:ease-in-out
-					 [&>ul>li>button]:w-full [&>ul>li>button]:text-left
-           [&>ul>li:hover]:bg-zinc-600"
 			use:menu.items
 			transition:fade={{ duration: 200 }}
 		>
-			<!-- Render the body snippet -->
-			{@render body()}
+			<List {body} />
 		</div>
 	{/if}
 </div>
-
-<!-- <style>
-	:global(.menu-list > ul) {
-		@apply my-3;
-	}
-	:global(.menu-list > ul > li) {
-		@apply flex items-center p-3 my-1 rounded-md gap-4 transition-colors duration-200 ease-in-out;
-	}
-
-	:global(.menu-list > ul > li:hover) {
-		@apply bg-zinc-600;
-	}
-</style> -->
