625069a0e80f195617a7bb1fa5f7974e7e0a79bf
Patrik Engborg <patrik@engborg.nu>
2025-02-05 09:16:03 +0100

    Remove redundant dependency ‘classnames’
    We can use native SvelteKit for this

--- src/lib/components/atoms/ConsoleLine.svelte
+++ src/lib/components/atoms/ConsoleLine.svelte
@@ -1,10 +1,9 @@
 <script>
-	import cn from 'classnames'
 	var { type = 'normal', children } = $props()
 </script>
 
 <pre
-	class={cn({
-		'text-primary-red': type === 'error',
-		'text-primary-yellow': type === 'warning'
-	})}>{@render children?.()}</pre>
+	class={[
+		type === 'error' && 'text-primary-red',
+		type === 'warning' && 'text-primary-yellow'
+	]}>{@render children?.()}</pre>
