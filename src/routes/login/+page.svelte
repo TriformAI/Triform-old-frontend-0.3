cdb1f7f8129d2d101ea15129c5c8e6ee6a57daac
Hannes Fant <hqnnes@gmail.com>
2025-02-10 20:37:36 +0200

    new login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,198 +1,64 @@
-<script>
-	import { signIn } from '@auth/sveltekit/client'
-	import { PUBLIC_API_URL } from '$env/static/public'
-	import Spinner from '$lib/components/Spinner.svelte'
-	import logo from '$lib/images/Logo.svg'
-	import github_mark_logo from '$lib/images/github-mark-white.svg'
+<script lang="ts">
+	import Button from '$lib/components/atoms/Button.svelte'
+
+	import { toast } from 'svelte-sonner'
 	import { onMount } from 'svelte'
 
-	let showPassword = false
-	let email = ''
-	let password = ''
-	let errorMessage = ''
-	let loading = false
-	const apiUrl = PUBLIC_API_URL
+	import DiscordIcon from '~icons/bxl/discord-alt'
+	import GithubIcon from '~icons/bxl/github'
 
-	async function handleLogin(event) {
-		event.preventDefault()
-		errorMessage = '' // Reset error message
-		loading = true // Show loading state
-		try {
-			const response = await fetch(`${apiUrl}/api/v1/login`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ email, password })
-			})
-
-			if (!response.ok) {
-				const errorData = await response.json()
-				console.log(errorData)
-				errorMessage = errorData.errors || 'Login failed.'
-			} else {
-				const data = await response.json()
-
-				// Set the token in a cookie
-				document.cookie = `authToken=${data.token}; Path=/; Max-Age=86400; SameSite=Strict`
-
-				//redirect to the dashboard
-				window.location.href = '/dashboard'
-
-				// You can adjust 'Max-Age' to control the cookie expiration time.
-				// Max-Age=86400 (seconds) sets it to expire after 1 day.
-				// 'SameSite=Strict' prevents it from being sent with cross-site requests.
-			}
-		} catch (error) {
-			console.log(error)
-			errorMessage = 'An error occurred. Please try again.'
-		} finally {
-			loading = false // Hide loading state
+	const providers = [
+		{
+			name: 'Discord',
+			icon: DiscordIcon
+		},
+		{
+			name: 'Github',
+			icon: GithubIcon
 		}
-	}
+	]
 
-	function handleLoginWithGithub() {
-		document.cookie = 'authType=login; path=/'
-		signIn('github', { callbackUrl: '/dashboard' })
+	const apiUrl = import.meta.env.VITE_TRICORE_URL
+
+	// Disable others
+	let chosenProvider = $state<string | null>(null)
+
+	const onLogin = (provider: typeof providers[number]) => {
+		chosenProvider = provider.name
+		// Return a promise that never resolves so the button starts loading while we're redirecting the user
+		return new Promise(() => {})
 	}
 
 	onMount(() => {
-		// go to the dashboard for now (no auth)
-		window.location.href = '/dashboard'
+		// Parse out error from query string
+		const error = new URLSearchParams(window.location.search).get('error')
+		console.log('error', error)
+		if (error) {
+			toast.error(`Error logging in: ${error}`)
+		}
 	})
 </script>
 
-<section>
-	<div class="flex h-screen flex-1 items-center justify-center bg-gray-900">
-		<div>
-			<a href="/">
-				<img alt="triform logo" src={logo} class="mx-auto mb-4 h-16 w-16" />
-			</a>
-			<div
-				class="animate-border w-full space-y-4 rounded-xl border bg-gray-950 p-10 shadow-xl md:w-[26em]"
+<div class="h-screen w-screen flex flex-col items-center justify-center bg-zinc-900">
+	<span class="text-zinc-400 text-center mb-2">
+		Continue with
+	</span>
+	<div class="flex flex-col items-center gap-y-4 w-full max-w-xs">
+		{#each providers as provider}
+			<Button
+				class="w-full"
+				href={`${apiUrl}/auth/login/${provider.name.toLowerCase()}`}
+				onClick={() => onLogin(provider)}
+				autoLoad={true}
+				disabled={chosenProvider === provider.name}
 			>
-				<form on:submit|preventDefault={handleLogin} id="login-form">
-					<div class="my-4 space-y-2">
-						<label for="email" class="block text-sm text-white">Email Address</label>
-						<input
-							autocomplete="email"
-							id="email"
-							type="email"
-							bind:value={email}
-							required
-							class="w-full rounded-lg bg-gray-800 p-2 text-white placeholder-gray-500"
-						/>
-						<span id="email-error" class="text-sm text-red-500"></span>
-					</div>
-					<div class="my-4 space-y-2">
-						<label for="password" class="block text-sm text-white">Password</label>
-						<div class="relative">
-							<input
-								id="password"
-								type={showPassword ? 'text' : 'password'}
-								bind:value={password}
-								required
-								class="w-full rounded-lg bg-gray-800 p-2 text-white placeholder-gray-500"
-							/>
-							<button
-								type="button"
-								aria-label="Toggle Password Visibility"
-								class="absolute inset-y-0 right-3 w-5"
-								on:click={() => (showPassword = !showPassword)}
-							>
-								{#if !showPassword}
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 576 512"
-										class="h-5 w-5 text-white"
-									>
-										<path
-											fill="currentColor"
-											d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z"
-										/>
-									</svg>
-								{:else}
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 640 512"
-										class="h-5 w-5 text-white"
-									>
-										<path
-											fill="currentColor"
-											d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm9.4 130.3C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5l-41.9-33zM192 256c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5z"
-										/>
-									</svg>
-								{/if}
-							</button>
-						</div>
-						<a href="/forgot-password" class="flex justify-end text-sm text-white hover:underline"
-							>Forgot Password</a
-						>
-						<span id="password-error" class="text-sm text-red-500"></span>
-					</div>
-
-					<button
-						type="submit"
-						class="mt-3 flex w-full items-center justify-center gap-x-2 rounded-lg bg-gray-800 py-2 text-white hover:bg-gray-700"
-						disabled={loading}
-					>
-						{#if loading}
-							<Spinner />
-						{:else}
-							<span id="login-text">Login</span>
-						{/if}
-					</button>
-
-					{#if errorMessage}
-						<p class="mt-2 text-center text-sm text-red-500">{errorMessage}</p>
-					{/if}
-				</form>
-				<div class="my-4 flex items-center justify-center">
-					<div class="flex-1 border-t border-gray-500"></div>
-					<span class="mx-4 text-gray-500">OR</span>
-					<div class="flex-1 border-t border-gray-500"></div>
-				</div>
-				<div>
-					<button
-						on:click={handleLoginWithGithub}
-						class="flex w-full items-center justify-center rounded-full border-4 border-gray-700 bg-gray-800 px-2 py-3 hover:bg-gray-700"
-					>
-						<div class="h-6 w-6">
-							<img src={github_mark_logo} alt="Github" class="h-full w-full object-contain" />
-						</div>
-						<p class="mx-5 font-mono text-xs font-bold tracking-tighter text-white sm:text-sm">
-							Login with Github
-						</p>
-					</button>
-
-					<h3 class="mt-3 truncate text-center text-xs text-gray-400">
-						Don't have an account yet? <a
-							href="/register"
-							class="cursor-pointer font-bold text-gray-200 hover:underline">Get Started</a
-						>
-					</h3>
-				</div>
-			</div>
-		</div>
+				{#snippet icon()}
+					<provider.icon />
+				{/snippet}
+				{#snippet body()}
+					{provider.name}
+				{/snippet}
+			</Button>
+		{/each}
 	</div>
-</section>
-
-<style>
-	@keyframes borderAnimation {
-		0% {
-			border-color: #833fb4;
-		}
-
-		50% {
-			border-color: #71c8ec;
-		}
-
-		100% {
-			border-color: #833fb4;
-		}
-	}
-
-	.animate-border {
-		animation: borderAnimation 4s infinite;
-	}
-</style>
+</div>
