e7f8ce90fed1eb3db1286ce0e75a5c698ccd5539
Hannes Fant <hannes@creavite.co>
2025-02-22 15:15:15 +0200

    fix creation of new components

--- src/routes/api/components/+server.ts
+++ src/routes/api/components/+server.ts
@@ -25,10 +25,11 @@ export async function PUT({ request, locals }) {
 
 	console.log('publishing component', data)
 
-	const updatedComponent = await locals.api.put<Component>(
-		`components/publish/${data.meta.id}`,
-		data
-	)
+	let updatedComponent
+	if (data.meta.version === 0)
+		updatedComponent = await locals.api.post<Component>('components/publish', data)
+	else
+		updatedComponent = await locals.api.put<Component>(`components/publish/${data.meta.id}`, data)
 
 	return json(updatedComponent)
 }
