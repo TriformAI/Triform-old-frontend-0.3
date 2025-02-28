ebe6b6c683b9f2f0f80b7a187833ee4998748494
Hannes Fant <hannes@creavite.co>
2025-02-28 13:47:04 +0200

    inputfield additions

--- src/lib/components/atoms/InputField.svelte
+++ src/lib/components/atoms/InputField.svelte
@@ -6,24 +6,38 @@
 		label,
 		name,
 		placeholder,
+		variation = 'default',
 		type = 'text',
 		autocomplete = 'off',
-		required
+		required,
+		class: classProp,
+		containerClass,
+		onblur,
+		onkeydown,
+		use = () => {}
 	}: {
 		value?: string
-		label: string
+		label?: string
 		name?: string
 		placeholder?: string
+		variation?: 'default' | 'tight'
 		type?: string
 		autocomplete?: FullAutoFill
 		required?: boolean
+		class?: string
+		containerClass?: string
+		onblur?: (e: FocusEvent) => void
+		onkeydown?: (e: KeyboardEvent) => void
+		use?: (el: HTMLInputElement) => void
 	} = $props()
 
 	const id = Math.random().toString(36).substring(2, 15)
 </script>
 
-<div>
-	<label for={id} class="text-sm font-medium">{label}</label>
+<div class={containerClass}>
+	{#if label}
+		<label for={id} class="text-sm font-medium">{label}</label>
+	{/if}
 	<input
 		{name}
 		{id}
@@ -32,6 +46,16 @@
 		{autocomplete}
 		{required}
 		bind:value
-		class="border-main-700 text-main-200 hover:border-main-600 focus:border-main-500 mt-2 w-full rounded-md border bg-transparent px-5 py-3 transition outline-none"
+		class={[
+			'border-main-700 text-main-200 hover:border-main-600 focus:border-main-500 rounded-md border bg-transparent',
+			'w-full transition outline-none',
+			label && 'mt-2',
+			variation === 'default' && 'px-5 py-3',
+			variation === 'tight' && 'px-2 py-1',
+			classProp
+		]}
+		{onblur}
+		{onkeydown}
+		use:use
 	/>
 </div>
