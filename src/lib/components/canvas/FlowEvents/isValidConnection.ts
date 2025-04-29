e6eff3c06ed7a5e8739fbc87206d69a95871e935
Hannes Fant <hannes@creavite.co>
2025-04-29 16:11:37 +0300

    prevent cycles w/ simple union find algo

--- src/lib/components/canvas/FlowEvents/isValidConnection.ts
+++ src/lib/components/canvas/FlowEvents/isValidConnection.ts
@@ -1,9 +1,31 @@
-import type { IsValidConnection as IsValidConnectionType, useSvelteFlow } from '@xyflow/svelte'
+import {
+	type IsValidConnection as IsValidConnectionType,
+	type useSvelteFlow,
+	getOutgoers
+} from '@xyflow/svelte'
+import type { Node } from '$lib/types/flow'
 
 export const isValidConnection: AddParameters<
 	IsValidConnectionType,
 	[ReturnType<typeof useSvelteFlow>]
 > = (connection, _useSvelteFlow) => {
-	console.log(connection)
-	return true
+	// no self-loops
+	if (connection.target === connection.source) return false
+	// always allow connections to the output handle in flows
+	if (connection.targetHandle?.endsWith(':output')) return true
+	// prevent cycles with union find
+	const { getNodes, getEdges } = _useSvelteFlow
+	const nodes = getNodes()
+	const edges = getEdges()
+	const hasCycle = (node: Node, visited: Set<Node['id']>) => {
+		if (visited.has(node.id)) return true
+		visited.add(node.id)
+		const outgoers = getOutgoers(node, nodes, edges)
+		for (const out of outgoers) {
+			if (out.id === connection.source || hasCycle(out as Node, visited)) return true
+		}
+	}
+	const targetNode = nodes.find(n => n.id === connection.target)
+	if (!targetNode) return false
+	return !hasCycle(targetNode as Node, new Set())
 }
