88566137055d91cbf05f914e1c5c99f9721250bf
Patrik Engborg <patrik@engborg.nu>
2025-05-05 13:03:12 +0200

    Fix overflow issue for project listing

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state'
 	import Navbar from '$lib/components/Navbar.svelte'
 	import 'balloon-css'
 
 	const { children } = $props()
+
+	const hideOverflow = $derived(page.url.pathname.startsWith('/project') && page.params.id) // On a project page
 </script>
 
-<div class="bg-main-900 grid h-dvh grid-rows-[auto_1fr]">
+<div class="grid h-dvh grid-rows-[auto_1fr]">
 	<Navbar />
 
-	<main class="overflow-hidden">
+	<main class={[hideOverflow && 'overflow-hidden']}>
 		{@render children()}
 	</main>
 </div>
