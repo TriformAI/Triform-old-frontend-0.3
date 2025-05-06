bdb7ea8b8782344027605d0f08442fb5feea13d9
Patrik Engborg <patrik@engborg.nu>
2025-05-06 09:50:04 +0200

    Add node actions to Panel

--- src/lib/components/panels/Panel.svelte
+++ src/lib/components/panels/Panel.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import type { Component, Snippet } from 'svelte'
-
+	import type { onClickFn } from '$lib/stores/nodeActions.svelte'
+	import { useSvelteFlow as useSvelteFlowHook } from '@xyflow/svelte'
 	import CodeEditor from './items/CodeEditor.svelte'
 	import Execute from './items/Execute.svelte'
 	import Metadata from './items/Metadata.svelte'
 	import ProjectSettings from './items/ProjectSettings.svelte'
 	import Variables from './items/Variables/Root.svelte'
-
 	import { selected } from '$lib/stores/canvas.svelte'
+	import { getActions } from '$lib/stores/nodeActions.svelte'
 
 	interface Props {
 		Icon?: Component
@@ -24,6 +25,14 @@
 		>
 	}
 
+	const actions = $derived(getActions(selected.node?.type ?? ''))
+
+	const useSvelteFlow = useSvelteFlowHook()
+
+	const handleActionClick = (fn: onClickFn) => {
+		fn(selected.node!, useSvelteFlow)
+	}
+
 	const { Icon, panels }: Props = $props()
 
 	const title = $derived(selected.node?.data.trinode.spec.meta.name ?? 'Project')
@@ -31,14 +40,36 @@
 </script>
 
 {#key selected.node?.id}
-	<div>
+	<div class="overflow-x-hidden">
 		<div class="border-b-main-800 mb-2 border-b px-3 pb-4">
-			<h2 class="flex items-center gap-2 text-lg font-semibold">
-				{#if Icon}
-					<Icon class="size-5" />
-				{/if}
-				{title}
-			</h2>
+			<div class="flex items-center gap-4">
+				<h2 class="flex items-center gap-2 truncate text-lg font-semibold">
+					{#if Icon}
+						<Icon class="size-5" />
+					{/if}
+					<span class="truncate">{title}</span>
+				</h2>
+
+				<ul class="ms-auto flex items-center gap-1">
+					{#each actions as action}
+						<li>
+							<button
+								aria-label={action.label}
+								data-balloon-pos="down"
+								onclick={() => handleActionClick(action.onClick)}
+								class={[
+									'rounded p-1 transition-colors',
+									action.isDangerous
+										? 'list-btn--danger hover:bg-danger-600/25'
+										: 'hover:bg-main-700 text-main-400 hover:text-main-200'
+								]}
+							>
+								<action.icon class="size-5" />
+							</button>
+						</li>
+					{/each}
+				</ul>
+			</div>
 
 			{#if desc}
 				<p class="text-main-400 mt-1 line-clamp-2">{desc}</p>
