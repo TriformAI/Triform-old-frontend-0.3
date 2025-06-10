22d0fcb86dae32c247c088e63ffd74afa3468d72
Hannes Fant <hannes@creavite.co>
2025-06-10 13:48:23 +0200

    fix recursive flow protection

--- src/lib/utils/flowHasComponent.ts
+++ src/lib/utils/flowHasComponent.ts
@@ -1,6 +1,7 @@
 import { type Component, type Flow, isFlow } from '$lib/types/agent'
 
 export const flowHasComponent = (flow: Flow, component: Component) => {
+	if (flow.meta.id === component.meta.id) return true
 	for (const node of Object.values(flow.spec.nodes)) {
 		// Check if it has the component as an immediate child
 		if (node.component_id === component.meta.id) return true
