0e53eda782b1f4d99934bab72adbee259c5ef780
Hannes Fant <hannes@creavite.co>
2025-06-14 13:37:05 +0200

    hide toaster if a dialog is open

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,21 @@
 	import '../app.css'
 	import { type Snippet } from 'svelte'
 	import { Toaster } from 'svelte-sonner'
+	import { browser } from '$app/environment'
 
 	let { children }: { children: Snippet } = $props()
+
+	const showToaster = $derived.by(() => {
+		if (!browser || !document) return true
+		// don't show any toasts here if there's an open dialog
+		const dialogs = document.querySelectorAll('dialog[open]')
+		console.log('dialogs', dialogs, !dialogs?.length)
+		return !!dialogs?.length
+	})
 </script>
 
 <div class="relative transform">{@render children?.()}</div>
 
-<Toaster richColors position="top-left" />
+{#if showToaster}
+	<Toaster richColors position="top-left" />
+{/if}
