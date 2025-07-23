88f703c9616d2aadee04cea54e341fb6ed9aab54
Patrik Engborg <patrik@engborg.nu>
2025-07-23 13:40:18 +0200

    Fix loading of project
    .

--- src/lib/components/panels/PanelItems.svelte
+++ src/lib/components/panels/PanelItems.svelte
@@ -39,6 +39,7 @@
 	}
 
 	const { items, componentData, showNav = true }: Props = $props()
+	$inspect(componentData)
 
 	// Get component type from resource
 	const componentType = $derived(componentData.resource.split('/')[0]) as keyof OpenPanelItems
--- src/routes/(app)/project/[id]/+layout.server.ts
+++ src/routes/(app)/project/[id]/+layout.server.ts
@@ -1,44 +1,24 @@
-import type { Payload, Project, Variable, Modifier, Trigger } from '$lib/types/project'
+import type { Project, Variable, Modifier, Trigger } from '$lib/types/resources'
 import { fail } from '@sveltejs/kit'
-import { db } from '$lib/db'
 import { getNodes } from '$lib/utils/getNodes'
-import { inArray } from 'drizzle-orm'
-import { drafts as draftsTable, nodePositions as nodePositionsTable } from '$lib/db/schema'
 import { isFlow, type Component, type Uuid } from '$lib/types/agent'
-import { objectMap } from '$lib/utils/objectMap'
 
 export async function load({ locals, params, depends }) {
 	depends('project')
 	const id = params.id
 	if (!id) return fail(404, { message: 'Project not found' })
 
-	const [project, modifiers, payloads, allComponents] = await Promise.all([
+	const [{ data: project }, { data: modifiers }, { data: components }] = await Promise.all([
 		locals.api.get<Project>(`projects/${id}?depth=999`),
 		locals.api.get<Modifier[]>(`modifiers?full=true`),
-		locals.api.get<Payload[]>(`payloads?full=true`),
+		//locals.api.get<Payload[]>(`payloads?full=true`),
 		locals.api.get<Component[]>(`components?full=true`)
 	])
 
-	const variables = modifiers.filter(m => m.resource === 'variable/v1') as Variable[]
+	const payloads = { data: [] }
+	const triggers = { data: [] as Trigger[] }
 
-	// we get all components (incl versions) for now, so we need to go through them
-	// and only get the latest version for each component
-	// for each componentId, find the one with the highest version
-	const getLatestVersion = (components: Component[]): Component[] => {
-		const grouped = Object.groupBy(components, c => c.meta.id) as Record<Uuid, Component[]>
-		return Object.values(grouped).map(group =>
-			group.reduce((latest, curr) => {
-				const latestVersion = latest.meta.version ?? 0
-				const currVersion = curr.meta.version ?? 0
-				return currVersion > latestVersion ? curr : latest
-			})
-		)
-	}
-	const triggers = getLatestVersion(
-		allComponents.filter(c => ['cron/v1', 'endpoint/v1'].includes(c.resource))
-	) as Trigger[]
-	// do the same for all components
-	const latestComponents = getLatestVersion(allComponents)
+	const variables = modifiers.filter(m => m.resource === 'variable/v1') as Variable[]
 
 	// Get the drafts & positions within each flow
 	const projectNodes = Object.values(project.spec.nodes).flatMap(node => [
@@ -54,26 +34,13 @@ export async function load({ locals, params, depends }) {
 		...flows.map(f => f.meta.id)
 	]
 
-	const [drafts, allPositions] = await Promise.all([
-		db.select().from(draftsTable).where(inArray(draftsTable.component_id, projectComponentIds)),
-		db.select().from(nodePositionsTable).where(inArray(nodePositionsTable.parent_id, parentIds))
-	])
-
-	const positions = objectMap(
-		// Group the positions by the parent flow
-		Object.groupBy(allPositions, p => p.parent_id!),
-		// make the positions an object like nodes instead of an array
-		nodes => Object.fromEntries(nodes?.map(n => [n.node_id, { x: n.x, y: n.y }]) ?? [])
-	)
+	const positions = { data: [] }
 
 	return {
 		project,
-		drafts: [...projectNodes.map(c => ({ component_id: c.meta.id, spec: c })), ...drafts] as {
-			component_id: Uuid
-			spec: Component
-		}[],
+		drafts: [],
 		positions,
-		components: latestComponents,
+		components,
 		modifiers,
 		variables,
 		payloads,
