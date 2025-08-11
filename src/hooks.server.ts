6a33cf2f611a6ca4de71109a11125b3b49c8e30a
Patrik Engborg <patrik@engborg.nu>
2025-08-11 09:43:21 +0200

    Remove old org stuff from hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { type Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
 import { API } from '$lib/api'
 import { env } from '$env/dynamic/private'
-import { base64 } from 'zod'
+
 const apiUrl = env.API_URL
 
 const authHandle: Handle = async ({ event, resolve }) => {
@@ -29,31 +29,6 @@ const authHandle: Handle = async ({ event, resolve }) => {
 	// Initialize API instance and make available to locals
 	const api = new API(apiUrl, event)
 	event.locals.api = api
-
-	try {
-		// TODO Get orgs from API
-		// const orgs = (await db
-		// 	.select({
-		// 		id: organizations.id,
-		// 		name: organizations.name,
-		// 		token: organizations.token,
-		// 		active: userOrganizations.active
-		// 	})
-		// 	.from(userOrganizations)
-		// 	.where(eq(userOrganizations.user_id, event.locals.user.id))
-		// 	.innerJoin(
-		// 		organizations,
-		// 		eq(userOrganizations.organization_id, organizations.id)
-		// 	)) as Organization[]
-		// event.locals.organizations = orgs
-		// // if we have an active org, update the auth token for the main api instance
-		// const activeOrg = orgs.find((org: Organization) => org.active)
-		// if (activeOrg) {
-		// 	event.locals.api = new API(baseUrl, `Bearer ${activeOrg.token}`, event.fetch)
-		// }
-	} catch (error) {
-		console.error('Failed fetching organizations for user', error)
-	}
 
 	return resolve(event)
 }
