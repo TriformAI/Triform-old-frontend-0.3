eaadbf8a4578444d4f0eb28e1ff03cb008f1df63
Patrik Engborg <patrik@engborg.nu>
2025-08-11 17:22:58 +0200

    Add @tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 import Icons from 'unplugin-icons/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		// @ts-expect-error expects plugin, but gets promise<plugin>
+		tailwindcss(),
 		sveltekit(),
 		// @ts-expect-error expects plugin, but gets promise<plugin>
 		Icons({
