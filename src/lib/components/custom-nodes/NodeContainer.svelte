cefdcb79ed0ab17849547be6eddb9c790bf8fddf
Patrik Engborg <patrik@engborg.nu>
2025-08-15 09:29:45 +0200

    Hide ports when on project

--- src/lib/components/custom-nodes/NodeContainer.svelte
+++ src/lib/components/custom-nodes/NodeContainer.svelte
@@ -4,7 +4,8 @@
 	import GhostHandle from './handles/GhostHandle.svelte'
 	import { Position } from '@xyflow/svelte'
 	import type { MetaNodeData, NodeData } from '$lib/types/canvas'
-
+	import { getCurrentContainer } from '$lib/stores/canvas.svelte'
+	import { isProject } from '$lib/schemas'
 	const {
 		body,
 		targetHandles,
@@ -20,28 +21,34 @@
 		showGhostTargetHandle?: boolean
 		data?: NodeData | MetaNodeData
 	} = $props()
+
+	const currentIsProject = $derived(isProject(getCurrentContainer()))
 </script>
 
 <div class="group/container relative w-full">
-	<div class={['mb-2 flex h-0 items-center justify-around gap-5']}>
-		{#each targetHandles as name}
-			<CustomHandle id={name} {name} type="target" position={Position.Top} />
-		{/each}
+	{#if !currentIsProject}
+		<div class={['mb-2 flex h-0 items-center justify-around gap-5']}>
+			{#each targetHandles as name}
+				<CustomHandle id={name} {name} type="target" position={Position.Top} />
+			{/each}
 
-		{#if showGhostTargetHandle}
-			<GhostHandle type="target" class="mt-4" />
-		{/if}
-	</div>
+			{#if showGhostTargetHandle}
+				<GhostHandle type="target" class="mt-4" />
+			{/if}
+		</div>
+	{/if}
 
 	{@render body()}
 
-	<div class={['flex items-center justify-around gap-5']}>
-		{#each sourceHandles as name}
-			<CustomHandle id={name} {name} type="source" position={Position.Bottom} />
-		{/each}
+	{#if !currentIsProject}
+		<div class={['flex items-center justify-around gap-5']}>
+			{#each sourceHandles as name}
+				<CustomHandle id={name} {name} type="source" position={Position.Bottom} />
+			{/each}
 
-		{#if showGhostSourceHandle}
-			<GhostHandle type="source" class="-mt-5" />
-		{/if}
-	</div>
+			{#if showGhostSourceHandle}
+				<GhostHandle type="source" class="-mt-5" />
+			{/if}
+		</div>
+	{/if}
 </div>
