de16a0d8a6aabcd8f7358c8f40f4fef86cf9e961
Hannes Fant <hannes@creavite.co>
2025-08-19 15:14:26 +0200

    fix middle of ghost node not being clickable

--- src/lib/components/custom-nodes/handles/GhostHandle.svelte
+++ src/lib/components/custom-nodes/handles/GhostHandle.svelte
@@ -16,6 +16,6 @@
 		position={type === 'source' ? Position.Bottom : Position.Top}
 		class="!bg-main-700 !border-main-400 z-10 flex size-6! items-center justify-center rounded-full border !border-dashed p-1.5"
 	>
-		<div class="bg-main-300 h-full w-full rounded-full"></div>
+		<div class="bg-main-300 pointer-events-none h-full w-full rounded-full"></div>
 	</Handle>
 </div>
