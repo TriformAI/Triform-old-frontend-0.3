12f49f677fa64b29864d7b3fe19035f2cf89bedb
Patrik Engborg <patrik@engborg.nu>
2025-08-21 11:05:23 +0200

    Make chat messages fit to content

--- src/lib/components/Chat/ChatMessage.svelte
+++ src/lib/components/Chat/ChatMessage.svelte
@@ -12,7 +12,7 @@
 	class={[
 		'relative',
 		item.role === 'user' &&
-			'text-main-200 bg-main-800 border-main-700 ms-auto max-w-3/4 rounded border p-4',
+			'text-main-200 bg-main-800 border-main-700 ms-auto w-fit max-w-3/4 rounded border p-3',
 		item.role === 'assistant' && 'text-main-400 mb-2'
 	]}
 >
