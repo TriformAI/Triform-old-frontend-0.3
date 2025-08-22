d4814dd940d85022bad23e61e151ee7bc85517f8
Hannes Fant <hannes@creavite.co>
2025-08-22 20:04:24 +0200

    fix triggers not visible when nesting

--- src/lib/components/panels/items/Triggers/Root.svelte
+++ src/lib/components/panels/items/Triggers/Root.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
 	import PanelItem from '../../PanelItem.svelte'
 	import Item from './ListItem.svelte'
-	import { page } from '$app/state'
-	import type { Trigger, Component } from '$lib/types/resources'
 	import Dialog from './Dialog.svelte'
-	import type * as z from 'zod'
-	import type { resolvedComponentModel, resolvedProjectModel } from '$lib/schemas'
-	import { getProject } from '$lib/stores/canvas.svelte'
+	import { getCurrentNodePath, getProject } from '$lib/stores/canvas.svelte'
 
 	let dialog = $state<HTMLDialogElement>()
 
 	const { nodeId }: { nodeId: string } = $props()
 
 	// triggers can only exist on top-level nodes
-	const node = $derived(getProject()?.spec.nodes[nodeId])
+	const node = $derived(
+		getProject()?.spec.nodes[nodeId === 'container' ? getCurrentNodePath().pop()! : nodeId]
+	)
 
 	// const triggers = $derived(
 	// 	page.data.triggers?.filter((t: Trigger) => t.spec.component_id === componentData.id)
