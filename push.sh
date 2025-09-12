ac0621577e3a60cf9d37a1d98636cc5f9bc71c69
Hannes Fant <hannes@creavite.co>
2025-09-12 12:40:23 +0200

    progress plain option

--- push.sh
+++ push.sh
@@ -1,1 +1,7 @@
-docker buildx build . --builder nexus-builder --platform linux/amd64 --tag rg.fr-par.scw.cloud/tf-prod/nexus-frontend:latest --push
+#!/bin/bash
+PROGRESS_FLAG=""
+if [ "$DEPLOY_SCRIPT_RUN" = "true" ]; then
+  PROGRESS_FLAG="--progress plain"
+fi
+
+docker buildx build . --builder nexus-builder --platform linux/amd64 --tag rg.fr-par.scw.cloud/tf-prod/nexus-frontend:latest --push $PROGRESS_FLAG
