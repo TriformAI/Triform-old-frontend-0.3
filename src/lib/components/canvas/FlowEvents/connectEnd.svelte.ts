666ad2a2092a4784f152ba3bf2321eda1bfc3a02
Hannes Fant <hannes@creavite.co>
2025-09-12 17:57:10 +0200

    fix ghost handle creation

--- src/lib/components/canvas/FlowEvents/connectEnd.svelte.ts
+++ src/lib/components/canvas/FlowEvents/connectEnd.svelte.ts
@@ -2,6 +2,7 @@ import { useSvelteFlow as svelteFlowHook } from '@xyflow/svelte'
 import type { OnConnectEnd } from '@xyflow/svelte'
 import { type CanvasNode } from '$lib/types/canvas'
 import { updateComponent } from '$lib/actions/components'
+import { canvasState } from '$lib/stores/canvas.svelte'
 
 import type { UUID as Uuid } from 'crypto'
 import {
@@ -12,7 +13,8 @@ import {
 	getCurrentContainer,
 	setEdges,
 	setNodes,
-	refreshFlow
+	refreshFlow,
+	addPort
 } from '$lib/stores/canvas.svelte'
 import { getNodeSelector } from '$lib/utils/getNodeSelector'
 import type { FinalConnectionState } from '@xyflow/system'
@@ -23,62 +25,71 @@ import { toast } from 'svelte-sonner'
 type ConnectEnd = AddParameters<OnConnectEnd, [ReturnType<typeof svelteFlowHook>]>
 
 async function handleGhostConnection(connectionState: FinalConnectionState) {
-	if (!connectionState.fromNode || !connectionState.fromHandle) {
-		return
+	if (!connectionState.fromNode || !connectionState.fromHandle || !connectionState.toHandle || !connectionState.toNode) return
+
+	let newHandle
+
+	// we have a couple cases:
+	// 2. from is a ghost port and we're dropping on a regular port
+	if (connectionState.fromHandle?.id?.startsWith('ghost-')) {
+		newHandle = connectionState.toHandle.id!
+		// 2.2 from is an IO node
+		// -> create a new input/output on the container
+		if (connectionState.fromNode.type === 'input-node')
+			await addPort('container', connectionState.toHandle.id!, {}, 'input')
+		else if (connectionState.fromNode.type === 'output-node')
+			await addPort('container', connectionState.toHandle.id!, {}, 'output')
+		// 2.1 from is a normal node
+		// -> create a new port on the from node
+		else
+			await addPort(connectionState.fromNode.id, connectionState.toHandle.id!, {}, connectionState.fromHandle.type === 'source' ? 'output' : 'input')
+	}
+	else {
+		if (!connectionState.toNode) return
+		newHandle = connectionState.fromHandle.id!
+		// 1. from is a regular port and we're dropping on a ghost port on:
+		// 1.2 an IO node
+		// -> create a new input/output on the container
+		if (connectionState.toNode?.type === 'input-node')
+			await addPort('container', connectionState.fromHandle.id!, {}, 'input')
+		else if (connectionState.toNode?.type === 'output-node')
+			await addPort('container', connectionState.fromHandle.id!, {}, 'output')
+		// 1.1 a normal node
+		// -> create a new port on the to node
+		else
+			await addPort(connectionState.toNode.id, connectionState.fromHandle.id!, {}, connectionState.toHandle.type === 'source' ? 'output' : 'input')
 	}
 
-	const currentContainer = getCurrentContainer() as
-		| z.infer<typeof resolvedFlowModel>
-		| z.infer<typeof resolvedAgentModel>
+	const source = connectionState.fromHandle.type === 'source'
+		? { id: connectionState.fromNode.id, handle: newHandle }
+		: { id: connectionState.toNode.id, handle: newHandle }
+	const target = connectionState.fromHandle.type === 'source'
+		? { id: connectionState.toNode.id, handle: newHandle }
+		: { id: connectionState.fromNode.id, handle: newHandle }
 
-	const isCreatingInput = connectionState.fromHandle!.type === 'target'
-
-	const fromComponent = $state.snapshot(getVisibleComponent(connectionState.fromNode.id))
-
-	const portKey = connectionState.fromHandle.id!
-
-	const specKey = isCreatingInput ? 'inputs' : 'outputs'
-
-	if (!(specKey in currentContainer.spec) || !(specKey in fromComponent.spec)) {
-		return
-	}
-
-	// Horrific conditional purely to make TS be quiet
-	if (isCreatingInput && 'inputs' in currentContainer.spec && 'inputs' in fromComponent.spec) {
-		currentContainer.spec.inputs[portKey] = fromComponent.spec.inputs[portKey]
-	} else if ('outputs' in currentContainer.spec && 'outputs' in fromComponent.spec) {
-		currentContainer.spec.outputs[portKey] = fromComponent.spec.outputs[portKey]
-	}
-
-	await updateComponent(currentContainer)
-	refreshFlow()
-
-	// Figure out which side is the source and which is the target
-	const source = {
-		id: isCreatingInput ? `${currentContainer.id}:input` : connectionState.fromNode?.id,
-		handle: portKey
-	}
-
-	const target = {
-		id: isCreatingInput ? connectionState.fromNode?.id : `${currentContainer.id}:output`,
-		handle: portKey
-	}
-
-	await addEdge(source, target)
+	addEdge(source, target)
 }
 
 async function handleRegularConnection(connectionState: FinalConnectionState) {
 	// Figure out which side is the source and which is the target
 	const fromIsTarget = connectionState.fromHandle!.type === 'target'
+	const sourceId = fromIsTarget ? connectionState.toNode?.id : connectionState.fromNode?.id
+	const sourceHandle = fromIsTarget ? connectionState.toHandle?.id : connectionState.fromHandle?.id
+	const targetId = fromIsTarget ? connectionState.fromNode?.id : connectionState.toNode?.id
+	const targetHandle = fromIsTarget ? connectionState.fromHandle?.id : connectionState.toHandle?.id
+
+	if (!sourceId || !sourceHandle || !targetId || !targetHandle) {
+		return toast.error('Invalid connection')
+	}
+
 	const source = {
-		id: fromIsTarget ? connectionState.toNode?.id : connectionState.fromNode?.id,
-		handle: fromIsTarget ? connectionState.toHandle?.id : connectionState.fromHandle?.id
+		id: sourceId,
+		handle: sourceHandle
 	}
 	const target = {
-		id: fromIsTarget ? connectionState.fromNode?.id : connectionState.toNode?.id,
-		handle: fromIsTarget ? connectionState.fromHandle?.id : connectionState.toHandle?.id
+		id: targetId,
+		handle: targetHandle
 	}
-	if (!source.id) return toast.error('Invalid source')
 
 	await addEdge(source, target)
 }
@@ -113,25 +124,23 @@ function handleVoidDrop(
 }
 
 export const handleConnectEnd: ConnectEnd = async (event, connectionState, useSvelteFlow) => {
+	canvasState.connecting = false
+	canvasState.connectingFrom = undefined
+
 	// Prevent connections between IO nodes
 	if (
 		['input', 'output'].includes(connectionState.fromHandle?.nodeId?.split(':')[1] ?? '') &&
 		['input', 'output'].includes(connectionState.toHandle?.nodeId?.split(':')[1] ?? '')
-	) {
-		return
-	}
+	) return
+
+	// show node sleector if dropped on empty space
+	if (!connectionState.isValid) return handleVoidDrop(connectionState, event, useSvelteFlow)
 
 	// if it's a valid connection, don't show the node selector but instead add the edge
-	if (connectionState.isValid) {
-		// Check if user is connectiong to a ghost port
-		if (connectionState?.toHandle?.id?.startsWith('ghost-')) {
-			return await handleGhostConnection(connectionState)
-		}
+	// Check if user is connecting to a ghost port
+	if (connectionState?.toHandle?.id?.startsWith('ghost-') || connectionState?.fromHandle?.id?.startsWith('ghost-'))
+		return await handleGhostConnection(connectionState)
 
-		return await handleRegularConnection(connectionState)
-	}
-
-	// Edge dropped on empty space, show node selector
-
-	handleVoidDrop(connectionState, event, useSvelteFlow)
+	// normal connection between ports
+	return await handleRegularConnection(connectionState)
 }
