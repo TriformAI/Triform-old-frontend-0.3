0659f17a7a5dc2ad3d84104108b8d9c98983fb9b
Hannes Fant <hannes@creavite.co>
2025-10-20 16:52:07 +0200

    fix widget again

--- src/lib/components/Chat/ChatWidget.svelte
+++ src/lib/components/Chat/ChatWidget.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { WidgetData, WidgetCompleteCallback } from '$lib/stores/chat.svelte'
+	import { onMount } from 'svelte'
 
 	const {
 		item,
@@ -9,7 +10,11 @@
 		onComplete?: WidgetCompleteCallback
 	} = $props()
 
-	const variablesPrompt = await import('./widgets/VariablesPrompt.svelte')
+	let variablesPrompt = $state()
+
+	onMount(async () => {
+		variablesPrompt = (await import('./widgets/VariablesPrompt.svelte')).default
+	})
 
 	const widgetMap = $derived({
 		variable_prompt: {
@@ -32,5 +37,7 @@
 	<p class="text-main-500">
 		{description}
 	</p>
-	<Widget {item} {onComplete} />
+	{#if Widget}
+		<Widget {item} {onComplete} />
+	{/if}
 </div>
