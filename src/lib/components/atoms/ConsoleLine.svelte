fc93ddab15f7c84ae29d83561208892c1433f560
Hannes Fant <hannes@creavite.co>
2025-11-05 14:14:26 +0100

    remove some old nafay components

--- src/lib/components/atoms/ConsoleLine.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	var { type = 'normal', children } = $props()
-</script>
-
-<pre
-	class={[
-		type === 'error' && 'text-rose',
-		type === 'warning' && 'text-warning-400'
-	]}>{@render children?.()}</pre>
